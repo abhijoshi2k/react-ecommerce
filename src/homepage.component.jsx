f6d4d13357f002abfcdc9669226fde45df67292d
ABHISHEK Joshi <abhijoshi2k@gmail.com>
2021-04-30 12:19:13 +0530

    add sass

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const HomePage = () => {
+const HomePage = () => (
 	<div className="homepage">
 		<div className="directory-menu">
 			<div className="menu-item">
@@ -34,7 +34,7 @@ const HomePage = () => {
 				</div>
 			</div>
 		</div>
-	</div>;
-};
+	</div>
+);
 
 export default HomePage;
