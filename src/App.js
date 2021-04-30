776378f3cc4e1ceb9ac8beda1dd5cb5dfa491ce3
ABHISHEK Joshi <abhijoshi2k@gmail.com>
2021-04-30 12:11:03 +0530

    updated packages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import HomePage from './homepage.component';
 
 function App() {
-	return <div className="App">Hello World</div>;
+	return (
+		<div className="App">
+			<HomePage />
+		</div>
+	);
 }
 
 export default App;
