c4022ff54942099be15044181aa2c5c7251e6493
ABHISHEK Joshi <abhijoshi2k@gmail.com>
2021-05-03 11:28:56 +0530

    Added basic router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+	<div>
+		<h1>HATS PAGE</h1>
+	</div>
+);
 
 function App() {
 	return (
 		<div className="App">
-			<HomePage />
+			<Switch>
+				<Route exact path="/" component={HomePage} />
+				<Route exact path="/hats" component={HatsPage} />
+			</Switch>
 		</div>
 	);
 }
